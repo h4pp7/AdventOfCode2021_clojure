0c24c26fa5c988e8075561be587e49ad22683506
Yann Wilhelm <yannwilhelm@yahoo.de>
2021-12-18 10:43:47 +0100

    draw function for day 17

--- src/advent_of_code/day_17.clj
+++ src/advent_of_code/day_17.clj
@@ -1,4 +1,19 @@
-(ns advent-of-code.day-17)
+(ns advent-of-code.day-17
+  (:require [clojure.string :refer [split split-lines includes? join]]))
+
+(defn diagram
+  [[x1 x2 y1 y2] pos]
+  (let [width (+ x2 10)
+        height (+ y2 3)]
+    (->> (for [y (range height)]
+           (->> (for [x (range width)]
+                  (cond 
+                    (some #{[x y]} pos) \●
+                    (and (< x1 x x2)
+                         (< y1 y y2))   \▒
+                    :else               \·))
+                (apply str))) 
+         (join \newline)))) 
 
 (defn part-1
   "Day 17 Part 1"
